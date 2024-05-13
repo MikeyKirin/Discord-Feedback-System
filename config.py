c35bc19ec33f91e843a3433b553c17c40b22577c
MikeyKirin <101678214+MikeyKirin@users.noreply.github.com>
2024-05-13 13:51:11 -0400

    Update config.py
    Adding clear indictators of info for devs to fill in.

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ import requests
 
 # Write out Discord configurations.
 API_ENDPOINT = 'https://discord.com/api/v10'
-TOKEN=""
-CLIENT_ID=""
-CLIENT_SECRET=""
+TOKEN="[YOUR BOT TOKEN HERE]"
+CLIENT_ID="[YOUR BOT CLIENT ID HERE]"
+CLIENT_SECRET="[YOUR BOT CLIENT SECRET HERE]"
 REDIRECT_URI="http://localhost:5000/oauth/discord"
-OAUTH_URL=""
+OAUTH_URL="[YOUR BOT OAUTH URL HERE]"
 
 # Write out database configurations
 host="localhost"
-database=""
-user="postgres"
-password=""
+database="yafsDB"
+user="[YOUR USERNAME HERE]"
+password="[YOUR PASSWORD HERE]"
 
 # Secret Key for sessions
-SECRET_KEY = "MikeysSuperSecretKey123"
+SECRET_KEY = "[YOUR SECRET KEY HERE]"
 
