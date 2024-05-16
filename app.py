ea961e8eca72fb66cee88e16d923ee757abda6a2
MikeyKirin <101678214+MikeyKirin@users.noreply.github.com>
2024-05-16 00:17:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import os
 # We've loaded all of our modules. Now time to load our secret data from our env file.
 
 # Write out Discord configurations.
-API_ENDPOINT='https://discord.com/api/v10'
-TOKEN=os.environ.get('TOKEN')
-CLIENT_ID=os.environ.get('CLIENT_ID')
-CLIENT_SECRET=os.environ.get('CLIENT_SECRET')
-REDIRECT_URI="https://discord-feedback-system.onrender.com/oauth/discord"
-OAUTH_URL=os.environ.get('OAUTH_URL')
+API_ENDPOINT = 'https://discord.com/api/v10'
+TOKEN = os.environ.get('TOKEN')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+REDIRECT_URI = "https://discord-feedback-system.onrender.com/oauth/discord"
+OAUTH_URL = os.environ.get('OAUTH_URL')
 
 # Write out database configurations
-HOST=os.environ.get('DB_HOST')
-DATABASE="yafsdb"
-USER="mikey"
-PASSWORD=os.environ.get('DB_PASSWORD')
+HOST = os.environ.get('DB_HOST')
+DATABASE = "yafsdb"
+USER = "mikey"
+PASSWORD = os.environ.get('DB_PASSWORD')
 
 # Secret Key for sessions
 SECRET_KEY = "SuperSecretKey123"
